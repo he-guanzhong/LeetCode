c3d1cad1547c2ae61248f398da9dc3123721770c
Your Name <guanzhong.he@outlook.com>
2024-06-23 21:53:21 +0800

    0623 DP

--- Code2/43-DP_lastStoneWeightII.cpp
+++ Code2/43-DP_lastStoneWeightII.cpp
@@ -1,5 +1,4 @@
 #include "head.h"
-#include <numeric>
 /* 1049. 最后一块石头的重量 II
 有一堆石头，用整数数组 stones 表示。其中 stones[i] 表示第 i 块石头的重量。
 每一回合，从中选出任意两块石头，然后将它们一起粉碎。假设石头的重量分别为 x 和
@@ -9,39 +8,40 @@ y，且 x <= y。那么粉碎的可能结果如下：
 最后，最多只会剩下一块 石头。返回此石头 最小的可能重量
 。如果没有石头剩下，就返回 0。
 示例 1：
-输入：stones = [2,7,4,1,8,1]
-输出：1
-解释：
-组合 2 和 4，得到 2，所以数组转化为 [2,7,1,8,1]，
-组合 7 和 8，得到 1，所以数组转化为 [2,1,1,1]，
-组合 2 和 1，得到 1，所以数组转化为 [1,1,1]，
-组合 1 和 1，得到 0，所以数组转化为 [1]，这就是最优值。
+  输入：stones = [2,7,4,1,8,1]
+  输出：1
+  解释：
+    组合 2 和 4，得到 2，所以数组转化为 [2,7,1,8,1]，
+    组合 7 和 8，得到 1，所以数组转化为 [2,1,1,1]，
+    组合 2 和 1，得到 1，所以数组转化为 [1,1,1]，
+    组合 1 和 1，得到 0，所以数组转化为 [1]，这就是最优值。
 示例 2：
-输入：stones = [31,26,33,21,40]
-输出：5
+  输入：stones = [31,26,33,21,40]
+  输出：5
     1 <= stones.length <= 30
-    1 <= stones[i] <= 100
- */
+    1 <= stones[i] <= 100 */
 
-// 允许石头共聚为一体，再碰撞减重。问题转化为石头分为总重相近的两堆，求其差。背包容量n为总重的一半，向下取整
-// dp[j]表示容量j下石头最大重量，背包遍历后，dp[n]为较小的一堆总重。返回较大的一堆和较小一堆总重差
-int lastStoneWeightII(vector<int> &stones) {
+int lastStoneWeightII(vector<int>& stones) {
   int sum = accumulate(stones.begin(), stones.end(), 0);
   int n = sum / 2;
   vector<int> dp(n + 1, 0);
   for (int i = 0; i < stones.size(); i++) {
-    for (int j = n; j >= stones[i]; j--)
+    for (int j = n; j >= stones[i]; j--) {
       dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]);
+    }
   }
   return sum - dp[n] * 2;
 }
+
+// 允许石头共聚为一体，再碰撞减重。问题转化为石头分为总重相近的两堆，求其差。背包容量n为总重的一半，向下取整
+// dp[j]表示容量j下石头最大重量，背包遍历后，dp[n]为较小的一堆总重。返回较大的一堆和较小一堆总重差
 
 // 核心是把石头分为重量接近的两堆，两堆重量之差就是剩余最小重量
 // dp[j]表示容量j内能容纳的最多石头体积。故weight和value均为stone
 // 考虑到石头总重不能超过30000，dp容量最多为其一般15001
 // target=sum/2向下取整了，故dp[target]必小于sum-dp[target]，输出其差
 // 时间复杂度m*n，m为石头总重一半，n为石头数量。空间复杂度m
-int lastStoneWeightII1(vector<int> &stones) {
+int lastStoneWeightII1(vector<int>& stones) {
   int sum = accumulate(stones.begin(), stones.end(), 0);
   int target = sum / 2;
   vector<int> dp(15001, 0);
@@ -51,6 +51,7 @@ int lastStoneWeightII1(vector<int> &stones) {
   }
   return sum - dp[target] - dp[target];
 }
+
 int main() {
   vector<int> nums1 = {2, 7, 4, 1, 8, 1};
   vector<int> nums2 = {31, 26, 33, 21, 40};
--- Code2/44-DP_findTargetSumWays.cpp
+++ Code2/44-DP_findTargetSumWays.cpp
@@ -1,5 +1,4 @@
 #include "head.h"
-#include <numeric>
 /* 494. 目标和
 给你一个非负整数数组 nums 和一个整数 target 。
 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
@@ -7,24 +6,21 @@
 ，然后串联起来得到表达式 "+2-1" 。
 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
 示例 1：
-输入：nums = [1,1,1,1,1], target = 3
-输出：5
-解释：一共有 5 种方法让最终目标和为 3 。
--1 + 1 + 1 + 1 + 1 = 3
-+1 - 1 + 1 + 1 + 1 = 3
-+1 + 1 - 1 + 1 + 1 = 3
-+1 + 1 + 1 - 1 + 1 = 3
-+1 + 1 + 1 + 1 - 1 = 3
+  输入：nums = [1,1,1,1,1], target = 3
+  输出：5
+  解释：一共有 5 种方法让最终目标和为 3 。
+  -1 + 1 + 1 + 1 + 1 = 3
+  +1 - 1 + 1 + 1 + 1 = 3
+  +1 + 1 - 1 + 1 + 1 = 3
+  +1 + 1 + 1 - 1 + 1 = 3
+  +1 + 1 + 1 + 1 - 1 = 3
 示例 2：
-输入：nums = [1], target = 1
-输出：1
-  */
+  输入：nums = [1], target = 1
+  输出：1  */
 
-// 回溯算法，改为已知负数一堆的总和为(sum-target)/2组合问题。注意排除两种意外情况，一是全为负也不行，即abs(target)>sum，二是sum-target为奇数.
-// 01背包，分正负两堆,已知和差。问题转化为已知正数堆和为背包总重，求填满背包种类数。dp[j]为重j背包填满的方法数。dp[j]+=dp[j-nums[i]]
-int findTargetSumWays(vector<int> &nums, int target) {
+int findTargetSumWays(vector<int>& nums, int target) {
   int sum = accumulate(nums.begin(), nums.end(), 0);
-  if (target > sum || (sum + target) % 2 == 1)
+  if (abs(target) > sum || (target + sum) % 2 == 1)
     return 0;
   int n = (sum + target) / 2;
   vector<int> dp(n + 1, 0);
@@ -37,11 +33,13 @@ int findTargetSumWays(vector<int> &nums, int target) {
   return dp[n];
 }
 
+// 回溯算法，改为已知负数一堆的总和为(sum-target)/2组合问题。注意排除两种意外情况，一是全为负也不行，即abs(target)>sum，二是sum-target为奇数.
+// 01背包，分正负两堆,已知和差。问题转化为已知正数堆和为背包总重，求填满背包种类数。dp[j]为重j背包填满的方法数。dp[j]+=dp[j-nums[i]]
 // 关键在于将所有元素总和分成left和right两堆，之和sum已知，两堆之差target已知。则left=(sum+target)/2，其必不可能为小数
 // 问题转化为在一系列元素中，已知某部分总和，求组合问题，可以使用回溯
 vector<vector<int>> result1;
 vector<int> path1;
-void backtracking1(vector<int> &nums, int target, int sum, int startIndex) {
+void backtracking1(vector<int>& nums, int target, int sum, int startIndex) {
   if (sum == target)
     result1.push_back(path1);
   for (int i = startIndex; i < nums.size() && sum + nums[i] <= target; i++) {
@@ -52,11 +50,12 @@ void backtracking1(vector<int> &nums, int target, int sum, int startIndex) {
     path1.pop_back();
   }
 }
-int findTargetSumWays1(vector<int> &nums, int target) {
+
+int findTargetSumWays1(vector<int>& nums, int target) {
   int sum = accumulate(nums.begin(), nums.end(), 0);
-  if (target > sum) // 差比总和还大，不可能
+  if (target > sum)  // 差比总和还大，不可能
     return 0;
-  if ((sum + target) % 2 == 1) // left部分是小数，不可能
+  if ((sum + target) % 2 == 1)  // left部分是小数，不可能
     return 0;
   int bagSize = (sum + target) / 2;
   result1.clear();
@@ -70,21 +69,22 @@ int findTargetSumWays1(vector<int> &nums, int target) {
 // 左堆的数量即背包容量。dp[j]表示填满j大小的包，总共几种方法。显然填满dp[0]只有一种办法。初始化dp[0]=1
 // 已经有nums[i]这个元素选定后，还剩余空间被填满方法是dp[j-nums[i]]种。递推公式dp[j]+=dp[j-nums[i]]
 // 遍历顺序，nums[i]放在外循环，包的容量放在内循环，且必需倒序
-int findTargetSumWays2(vector<int> &nums, int target) {
+int findTargetSumWays2(vector<int>& nums, int target) {
   int sum = accumulate(nums.begin(), nums.end(), 0);
-  if (abs(target) > sum) // 最大差值比总和还大，不可能
+  if (abs(target) > sum)  // 最大差值比总和还大，不可能
     return 0;
   if ((sum - target) % 2 == 1)
     return 0;
   int bagSize = (sum - target) / 2;
   vector<int> dp(bagSize + 1, 0);
-  dp[0] = 1; // 初始化一定是设为1
+  dp[0] = 1;  // 初始化一定是设为1
   for (int i = 0; i < nums.size(); i++) {
     for (int j = bagSize; j >= nums[i]; j--)
       dp[j] += dp[j - nums[i]];
   }
   return dp[bagSize];
 }
+
 int main() {
   vector<int> nums1 = {1, 1, 1, 1, 1};
   vector<int> nums2 = {1};
@@ -93,4 +93,5 @@ int main() {
        << findTargetSumWays(nums2, target2) << endl;
   cout << findTargetSumWays2(nums1, target1) << " "
        << findTargetSumWays2(nums2, target2) << endl;
+  return 0;
 }
