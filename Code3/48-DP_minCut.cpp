67a2d2178c880b53f77af867e0ea75b522e67305
Your Name <guanzhong.he@outlook.com>
2024-08-04 21:39:55 +0800

    0804 DP

---
+++ Code3/48-DP_minCut.cpp
@@ -0,0 +1,46 @@
+#include "head.h"
+/* 132. 分割回文串 II
+给你一个字符串 s，请你将 s 分割成一些子串，使每个子串都是回文串。
+返回符合要求的 最少分割次数 。
+示例 1：
+  输入：s = "aab"
+  输出：1
+  解释：只需一次分割就可将 s 分割成 ["aa","b"] 这样两个回文子串。
+示例 2：
+  输入：s = "a"
+  输出：0
+示例 3：
+  输入：s = "ab"
+  输出：1 */
+
+// 先求所有子串是否回文矩阵。[0,i]内最少分割的回文次数，取决于[0,j]内最小次数，和[j+1,i]是不是回文串，如果是，则切割一次dp[j]+1
+// 初始化dp[i]=i，因为最差的情况就是每个字符一切割。i从小到大顺序遍历，注意如何[0,i]直接回文，dp[i]=0
+int minCut(string s) {
+  vector<vector<bool>> isPalindrome(s.size(), vector<bool>(s.size(), 0));
+  for (int i = s.size() - 1; i >= 0; i--) {
+    for (int j = i; j < s.size(); j++) {
+      if (s[i] == s[j] && (j - i < 3 || isPalindrome[i + 1][j - 1]))
+        isPalindrome[i][j] = true;
+    }
+  }
+  vector<int> dp(s.size(), 0);
+  for (int i = 0; i < s.size(); i++)
+    dp[i] = i;
+  for (int i = 1; i < s.size(); i++) {
+    if (isPalindrome[0][i]) {
+      dp[i] = 0;
+      continue;
+    }
+    for (int j = 0; j < i; j++) {
+      if (isPalindrome[j + 1][i])
+        dp[i] = min(dp[i], dp[j] + 1);
+    }
+  }
+  return dp[s.size() - 1];
+}
+
+int main() {
+  string s1 = "aab", s2 = "a", s3 = "ab";
+  cout << minCut(s1) << " " << minCut(s2) << " " << minCut(s3) << endl;
+  return 0;
+}
--- Code4/51_DP_longestPalindrome.cpp
+++ Code4/51_DP_longestPalindrome.cpp
@@ -3,47 +3,67 @@
 给你一个字符串 s，找到 s 中最长的回文子串。
 如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
 示例 1：
-输入：s = "babad"
-输出："bab"
-解释："aba" 同样是符合题意的答案。
+  输入：s = "babad"
+  输出："bab"
+  解释："aba" 同样是符合题意的答案。
 示例 2：
-输入：s = "cbbd"
-输出："bb" */
+  输入：s = "cbbd"
+  输出："bb" */
 
 string longestPalindrome(string s) {
-  int left = 0, len = 0;
-  vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+  int start = 0, len = 0;
+  vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), 0));
   for (int i = s.size() - 1; i >= 0; i--) {
     for (int j = i; j < s.size(); j++) {
-      if (s[i] == s[j])
-        dp[i][j] = (j - i < 3) ? true : dp[i + 1][j - 1];
-      if (dp[i][j] && j - i + 1 > len) {
-        left = i;
-        len = j - i + 1;
+      if (s[i] == s[j]) {
+        dp[i][j] = j - i <= 2 ? true : dp[i + 1][j - 1];
+        if (dp[i][j] && j - i + 1 > len) {
+          len = j - i + 1;
+          start = i;
+        }
       }
     }
   }
-  return s.substr(left, len);
+  return s.substr(start, len);
 }
 
 // dp[i][j]表示下标区间[i.j]的字符串是否为回文。额外记录最终应该返回的，起始点left和长度len，从左下至右上遍历。
 // 一次判断，子串首尾是否相等，如相等，分两种情况讨论：长度小于等于3，直接为真，长度大于3，要看[i+1,j+1]的值
 // 二次判断，dp[i][j]是否为真，为真则看该子串长度是否大于原值，如更长，则记录新子串起始点和长度。最终返回目标子串。注意，一二次遍历不能混同处理
-string longestPalindrome1(string s) {
+string longestPalindrome2(string s) {
   vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
-  int left = 0, len = 0;
+  int left = 0, maxLen = 0;
   for (int i = s.size() - 1; i >= 0; i--) {
     for (int j = i; j < s.size(); j++) {
       if (s[i] == s[j]) {
         dp[i][j] = (j - i < 3) ? true : dp[i + 1][j - 1];
-        if (dp[i][j] && j - i + 1 > len) {
-          len = j - i + 1;
+        if (dp[i][j] && j - i + 1 > maxLen) {
+          maxLen = j - i + 1;
           left = i;
         }
       }
     }
   }
-  return s.substr(left, len);
+  return s.substr(left, maxLen);
+}
+
+// 动态规划时间和空间复杂度均为O(n^2),双指针可以将空间复杂度将为O(1)
+void extend1(const string& s, int i, int j, int& left, int& maxLen) {
+  while (i >= 0 && j < s.size() && s[i] == s[j]) {
+    if (j - i + 1 > maxLen) {
+      maxLen = j - i + 1;
+      left = i;
+    }
+    i--, j++;
+  }
+}
+string longestPalindrome1(string s) {
+  int left = 0, maxLen = 0;
+  for (int i = 0; i < s.size(); i++) {
+    extend1(s, i, i, left, maxLen);
+    extend1(s, i, i + 1, left, maxLen);
+  }
+  return s.substr(left, maxLen);
 }
 
 int main() {
